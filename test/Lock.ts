be2aeceea876c8a7d91ba4100567d9a918a91063
rayedsikder <rayedsikder33@gmail.com>
2023-10-31 16:52:55 +0600

    [WIP] Add test

--- test/Lock.ts
+++ test/Lock.ts
@@ -6,29 +6,24 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Lock", function () {
+describe("HashStressTest", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+  async function deploy() {
 
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [owner] = await ethers.getSigners();
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const Hash = await ethers.getContractFactory("Hash");
+    const hash = await Hash.deploy();
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    return { hash };
   }
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+  describe("InputTest", function () {
+    it("Should set the hash 10000 times ", async function () {
+      const { hash } = await loadFixture(deploy);
 
       expect(await lock.unlockTime()).to.equal(unlockTime);
     });
